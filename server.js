ecfe1d7fa53b1503a2447d6c0ecfb968d23ab83b
Rishav Salodhia <rishav.salodhia@nagarro.com>
2019-04-25 12:05:15 +0530

    added angular compiler

--- server.js
+++ server.js
@@ -4,11 +4,11 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist/weatherAssignment'));
+app.use(express.static(__dirname + '/dist/my-test-app'));
 
 app.get('/*', function(req,res) {
     
-res.sendFile(path.join(__dirname+'/dist/weatherAssignment/index.html'));
+res.sendFile(path.join(__dirname+'/dist/my-test-app/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
